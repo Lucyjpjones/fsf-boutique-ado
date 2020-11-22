c705ba856791150ff72d6301a32dcfaf35173d3e
Lucyjpjones <lucyjpjones@gmail.com>
2020-11-22 15:57:40 +0000

    Removed secret key and set debug

---
+++ Procfile
@@ -0,0 +1,1 @@
+wed: gunicorn boutique_ado.wsgi:application
--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -18,10 +18,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$w^(@kzug54org9po*emeo90kud66v9zlcb(4emrf(ehgwowe9'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['lj-boutique-ado.herokuapp.com', 'localhost']
 
